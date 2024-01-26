f8e38b98f61396ba577205d9886517b048c21db0
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 21:48:05 +0800

    description

---
+++ exercise_11.py
@@ -0,0 +1,7 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 11:
+# Write a Program to extract each digit from an integer in the reverse order.
+# For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
