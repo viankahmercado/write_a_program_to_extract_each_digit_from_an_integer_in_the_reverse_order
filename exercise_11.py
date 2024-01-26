a7aad735e623e7c4f587a050c2a33c534aa5c771
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 21:54:43 +0800

    pseudocode

--- exercise_11.py
+++ exercise_11.py
@@ -5,3 +5,7 @@
 # Exercise 11:
 # Write a Program to extract each digit from an integer in the reverse order.
 # For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
+
+#function containing while loop
+
+# check by printing the variable
